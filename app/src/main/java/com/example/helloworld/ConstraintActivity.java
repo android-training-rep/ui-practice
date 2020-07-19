20398640d014ac3808c4b8d544def556be8b5b4f
Liao Xueqing <xueqingliao@foxmail.com>
2020-07-19 15:32:19 +0800

    Add ui element

--- app/src/main/java/com/example/helloworld/ConstraintActivity.java
+++ app/src/main/java/com/example/helloworld/ConstraintActivity.java
@@ -2,13 +2,29 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConstraintActivity extends AppCompatActivity {
 
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint);
+        loginBtn = findViewById(R.id.login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewPage();
+            }
+        });
+    }
+
+    private void openNewPage() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
